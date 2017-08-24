2603c0e0f52159cbcb1811940cb29e9fd25ffe85
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-24 15:37:44 -0700

    start setting up cart display

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+//for every item in the orderedProducts array do this process:
+for (var i = 0; i < orderedProducts.length; i++){
+//create div to hold ordered product info and add to body
+  var body = document.getElementsByTagName('body')[0];
+  var div = document.createElement('div');
+  body.appendChild(div);
+}
