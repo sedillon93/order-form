c027d9ac8f903079bcdbc81c4e6d93838fc1e55a
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-24 16:12:57 -0700

    change variable names to not use hyphens

--- app.js
+++ app.js
@@ -15,26 +15,27 @@ var breakfast = new Product('All-in-1 Breakfast','breakfast', 'img/breakfast.jpg
 var bubblegum = new Product('Meatball bubblegum', 'bubblegum', 'img/bubblegum.jpg');
 var chair = new Product('Red chair', 'chair', 'img/chair.jpg');
 var cthulhu = new Product('Cthulhu', 'cthulhu', 'img/cthulhu.jpg');
-var dog-duck = new Product('Dog duckbill', 'dog-duck', 'img/dog-duck.jpg');
+var dogDuck = new Product('Dog duckbill', 'dog-duck', 'img/dog-duck.jpg');
 var dragon = new Product('Dragon meat', 'dragon', 'img/dragon.jpg');
 var pen = new Product('Utensil pen caps', 'pen', 'img/pen.jpg');
-var pet-sweep = new Product('Pet sweeper shoes','pet-sweep', 'img/pet-sweep.jpg');
+var petSweep = new Product('Pet sweeper shoes','pet-sweep', 'img/pet-sweep.jpg');
 var scissors = new Product('Pizza scissors','scissors', 'img/scissors.jpg');
 var shark = new Product('Shark sleeping bag', 'shark', 'img/shark.jpg');
 var sweep = new Product('Baby sweeper pajamas', 'sweep', 'img/sweep.png');
 var tauntaun = new Product('Tauntaun sleeping bag', 'tauntaun', 'img/tauntaun.jpg');
 var unicorn = new Product('Unicorn meat', 'unicorn', 'img/unicorn.jpg');
 var usb = new Product('Octopus usb drive', 'usb', 'img/usb.gif');
-var water-can = new Product('Artistic watering can', 'water-can', 'img/water-can.jpg');
-var wine-glass = new Product('Wine glass', 'wine-glass', 'img/wine-glass.jpg');
+var waterCan = new Product('Artistic watering can', 'water-can', 'img/water-can.jpg');
+var wineGlass = new Product('Wine glass', 'wine-glass', 'img/wine-glass.jpg');
 
-var products = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dog-duck, dragon, pen, pet-sweep, scissors, shark, sweep, tauntaun, unicorn, usb, water-can, wineglass];
+var products = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
 
 //for every item in the orderedProducts array do this process:
 for (var i = 0; i < orderedProducts.length; i++){
 //create div to hold ordered product info and add to body
-  var body = document.getElementsByTagName('body')[0];
+  var body = document.getElementById('cart');
   var div = document.createElement('div');
+  div.innerText = 'success?';
   body.appendChild(div);
 //create img element for product image
   var image = document.createElement('img');
