8e379064f43691ba8336d2354f87185e2f73a6b0
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-24 15:48:16 -0700

    add product constructor function and product array to js file

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 'use strict';
 
+function Product(name, id, path) {
+  this.name = name;
+  this.id = id;
+  this.path = path;
+  this.clicks = 0;
+  this.shown = 0;
+}
+var bag = new Product('Bag', 'bag', 'img/bag.jpg');
+var banana = new Product('Banana Slicer', 'banana', 'img/banana.jpg');
+var bathroom = new Product('Bathroom appliance','bathroom', 'img/bathroom.jpg');
+var boots = new Product('Boots','boots', 'img/boots.jpg');
+var breakfast = new Product('All-in-1 Breakfast','breakfast', 'img/breakfast.jpg');
+var bubblegum = new Product('Meatball bubblegum', 'bubblegum', 'img/bubblegum.jpg');
+var chair = new Product('Red chair', 'chair', 'img/chair.jpg');
+var cthulhu = new Product('Cthulhu', 'cthulhu', 'img/cthulhu.jpg');
+var dogDuck = new Product('Dog duckbill', 'dogDuck', 'img/dogDuck.jpg');
+var dragon = new Product('Dragon meat', 'dragon', 'img/dragon.jpg');
+var pen = new Product('Utensil pen caps', 'pen', 'img/pen.jpg');
+var petSweep = new Product('Pet sweeper shoes','petSweep', 'img/petSweep.jpg');
+var scissors = new Product('Pizza scissors','scissors', 'img/scissors.jpg');
+var shark = new Product('Shark sleeping bag', 'shark', 'img/shark.jpg');
+var sweep = new Product('Baby sweeper pajamas', 'sweep', 'img/sweep.png');
+var tauntaun = new Product('Tauntaun sleeping bag', 'tauntaun', 'img/tauntaun.jpg');
+var unicorn = new Product('Unicorn meat', 'unicorn', 'img/unicorn.jpg');
+var usb = new Product('Octopus usb drive', 'usb', 'img/usb.gif');
+var waterCan = new Product('Artistic watering can', 'waterCan', 'img/waterCan.jpg');
+var wineGlass = new Product('Wine glass', 'wineGlass', 'img/wineGlass.jpg');
+
+var products = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
+
 //for every item in the orderedProducts array do this process:
 for (var i = 0; i < orderedProducts.length; i++){
 //create div to hold ordered product info and add to body
   var body = document.getElementsByTagName('body')[0];
   var div = document.createElement('div');
   body.appendChild(div);
+//create img element for product image
+  var image = document.createElement('img');
+  //replace imagePath with correct path for images
+  image.setAttribute('src', imagePath);
 }
