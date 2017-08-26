0f736c94b7aa90cf04afc2ab53b143dd42cd5059
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-26 10:29:35 -0700

    update orderedProd array on page and in local storage when removeItem button is clicked

--- cart.js
+++ cart.js
@@ -35,7 +35,12 @@ function ordersPresent(){
   if (localStorage.getItem('ordered')){
     orderedProducts = JSON.parse(localStorage.getItem('ordered'));
   }
-
+  // else {
+  //   var body = document.getElementById('cart');
+  //   var p = document.createElement('p');
+  //   p.innerText = 'Your cart is empty. To select a product go back to the order form.';
+  //   body.appendChild(p);
+  // }
 }
 ordersPresent();
 // for every item in the orderedProducts array do this process:
@@ -71,6 +76,7 @@ function deleteItem(event){
       var item = document.getElementsByClassName(orderedProducts[i].id)[0];
       body.removeChild(item);
       orderedProducts.splice(i, 1);
+      localStorage.setItem('ordered', JSON.stringify(orderedProducts));
       location.reload();
     }
   }
