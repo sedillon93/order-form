2401eaa6a26dae6e5a9209a741b9943e28111395
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-25 17:52:38 -0700

    fix persistence between cart and order form pages for objects removed

--- cart.js
+++ cart.js
@@ -73,9 +73,8 @@ function deleteItem(event){
       var item = document.getElementsByClassName(orderedProducts[i].id)[0];
       body.removeChild(item);
       orderedProducts.splice(i, 1);
-      console.log(orderedProducts.splice(i, 1));
       var test = localStorage.setItem('ordered', JSON.stringify(orderedProducts));
-      console.log(test);
+      location.reload();
     }
   }
 }
